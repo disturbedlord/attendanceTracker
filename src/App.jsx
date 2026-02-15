20880d007d16299bfb21a5399ac7ac560ea615da
Avinash K <todisturbedlord@gmail.com>
2026-02-15 11:26:34 +0530

    Modified config

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import MarkAttendance from "./pages/MarkAttendance";
@@ -12,38 +12,42 @@ import AppLayout from "./components/layout/AppLayout";
 function App() {
   return (
     <div>
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route path="/" element={<Login />} />
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute allowedRoles={["teacher", "admin", "superadmin"]}>
-                <DashboardRedirect />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="/class/:id" element={<ClassDetails />} />
-          <Route path="/class/:id/attendance" element={<MarkAttendance />} />
-          <Route path="/accept-invite" element={<AcceptInvite />} />
-          <Route
-            path="/teacher"
-            element={
-              <ProtectedRoute allowedRoles={["teacher"]}>
-                <TeacherDashboard />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/teacher/class/:classId"
-            element={
-              <ProtectedRoute allowedRoles={["teacher"]}>
-                <ClassDetails />
-              </ProtectedRoute>
-            }
-          />
-        </Route>
-      </Routes>
+      <HashRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route path="/" element={<Login />} />
+            <Route
+              path="/dashboard"
+              element={
+                <ProtectedRoute
+                  allowedRoles={["teacher", "admin", "superadmin"]}
+                >
+                  <DashboardRedirect />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/class/:id" element={<ClassDetails />} />
+            <Route path="/class/:id/attendance" element={<MarkAttendance />} />
+            <Route path="/accept-invite" element={<AcceptInvite />} />
+            <Route
+              path="/teacher"
+              element={
+                <ProtectedRoute allowedRoles={["teacher"]}>
+                  <TeacherDashboard />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/teacher/class/:classId"
+              element={
+                <ProtectedRoute allowedRoles={["teacher"]}>
+                  <ClassDetails />
+                </ProtectedRoute>
+              }
+            />
+          </Route>
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
